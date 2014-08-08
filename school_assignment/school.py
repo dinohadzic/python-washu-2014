10bf31d18649cebcf779a5c7b5ce6c4db31a3358
dinohadzic <dino.hadzic@wustl.edu>
2014-08-07 22:58:22 -0500

    Adding documentation to homework assignment 1

--- school_assignment/school.py
+++ school_assignment/school.py
@@ -5,17 +5,23 @@ class School():
 		
 	def add(self, name, grade):
 		if grade in self.db:
-			self.db[grade].add(name)
-		else: self.db[grade] = {name}
-		
+			self.db[grade].add(name)	# If grade already exists, adds name to the grade.
+		else: self.db[grade] = {name}	# If grade does not exist, creates the grade 
+											#and adds name to it.
+	
 	def sort(self):
 		sorted_students = {}
 		for i in self.db.keys():
-			sorted_students[i] = tuple(sorted(self.db[i]))
-		return sorted_students
+			sorted_students[i] = tuple(sorted(self.db[i])) 
+		return sorted_students	# Loops through the keys of db (grade), and creates 
+									#entries in sorted_students, where the grade is the 
+									#key, and the tuple of students (now sorted 
+									#alphabetically) are the values.							
 			
 	def grade(self, grade):
 		if grade in self.db: 
 			return self.db[grade]
-		else: return None
+		else: return None	# If the grade exists, returns names of the students 
+								#in that grade. If the grade doesn't exist, 
+								#returns "None."
 		
