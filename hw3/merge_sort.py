9581ee4f3a3d81d59d0ce8eba6cf7e58a024ab57
dinohadzic <dino.hadzic@wustl.edu>
2014-08-14 01:42:30 -0500

    adding documentation to merge_sort.py

--- hw3/merge_sort.py
+++ hw3/merge_sort.py
@@ -1,8 +1,8 @@
 def merge_sort(list):
-	if len(list) <= 1:
+	if len(list) <= 1: #If the length of the input is 1, simply return that value
 		return list
 	else:
-		mid = len(list)/2
+		mid = len(list)/2 
 		left = list[:mid]
 		right = list[mid:]
 		
@@ -14,14 +14,14 @@ def merge_sort(list):
 		i = 0
 		j = 0
 		
-		while i < len(left) and j < len(right):
-			if left[i] < right[j]:
+		while i < len(left) and j < len(right): 
+			if left[i] < right[j]: #Should the left be smaller than the right, append it to the list
 				sorted_list.append(left[i])
 				i += 1
 			else:
-				sorted_list.append(right[j])
+				sorted_list.append(right[j]) #Should the right be smaller than the left, append it to the list
 				j += 1
-			if i == len(left) or j == len(right):
+			if i == len(left) or j == len(right): #If the end is reached on the left (or right), extends the list by what's remaining of the right (or left)
 				sorted_list.extend(left[i:] or right[j:])	
 		return sorted_list		
 
